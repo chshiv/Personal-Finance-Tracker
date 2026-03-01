45fb8ef45d92e1bdad7ec083bfff279ec2fe0e56
Shiv <shivchoudhary369@gmail.com>
2026-03-02 01:01:52 +0530

    update token expire

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -85,7 +85,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: '3h' }
+      { expiresIn: '8h' }
     );
 
     res.status(200).json({ token, role: user.role, name: user.name });
