86debb275067a455a4aa1c5dab7e3a1285c9205f
Shiv <shivchoudhary369@gmail.com>
2026-03-02 01:03:35 +0530

    add filter route

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getTransactions, createTransaction, updateTransaction, deleteTransaction } from '../controllers/transactionController.js';
 import { authMiddleware } from '../middlewares/auth.js';
+import { filterTransactions } from '../controllers/transactionController.js';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.get('/', authMiddleware(['admin', 'user', 'read-only']), getTransactions)
 router.post('/', authMiddleware(['admin','user']), createTransaction);
 router.put('/:id', authMiddleware(['admin','user']), updateTransaction);
 router.delete('/:id', authMiddleware(['admin','user']), deleteTransaction);
+router.get('/filter', authMiddleware(['admin', 'user', 'read-only']), filterTransactions);
 
 export default router;
